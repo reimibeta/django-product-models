867beba2fb54931052e38496b2543ef33f92db1e
reimibeta <65539218+reimibeta@users.noreply.github.com>
2021-06-08 20:15:26 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,19 +11,6 @@ import setuptools
 
 setuptools.setup(
     name='product_models',
-    version='1.0.1',
-    packages=setuptools.find_packages(),
-    # install_requires=[
-    #     'image-utils @ git+https://github.com/reimibeta/django-image-utils.git',
-    #     'datetime-utils @ git+https://github.com/reimibeta/django-datetime-utils.git',
-    #     'html-render-utils @ git+https://github.com/reimibeta/django-html-render-utils.git',
-    #     'djangorestframework==3.12.4',
-    #     'djangorestframework-simplejwt==4.7.0',
-    #     'drf-flex-fields==0.9.0',
-    #     'django-admin-list-filter-dropdown==1.0.3',
-    #     'rest-framework-utils @ git+https://github.com/reimibeta/django-rest-framework-utils.git',
-    #     'pillow==8.2.0',
-    #     'django-cleanup==5.2.0'
-    # ]
-    # scripts=['makemigrations.py','migrate.py']
+    version='1.0.2',
+    packages=setuptools.find_packages()
 )
