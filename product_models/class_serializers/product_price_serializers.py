16ffb66e7cda04dee4a29d777831395ad03c2171
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-06-08 15:55:21 +0700

    .

--- product_models/class_serializers/product_price_serializers.py
+++ product_models/class_serializers/product_price_serializers.py
@@ -1,4 +1,5 @@
 from rest_flex_fields import FlexFieldsModelSerializer
+from rest_framework import serializers
 
 from product_models.class_models.product_price import ProductPrice, ProductCurrency
 
@@ -12,8 +13,26 @@ class ProductCurrencySerializer(FlexFieldsModelSerializer):
 
 
 class ProductPriceSerializer(FlexFieldsModelSerializer):
+    currency = serializers.PrimaryKeyRelatedField(
+        queryset=ProductCurrency.objects.all(),
+        write_only=True
+    )
+
+    price = serializers.DecimalField(
+        write_only=True,
+        max_digits=20,
+        decimal_places=2,
+        error_messages={'blank': 'INVALID!!11', 'null': 'NULL11!'}
+        # style={'input_type': 'password'}
+    )
+
+    product_price = serializers.SerializerMethodField('product_price_define')
+
+    def product_price_define(self, obj):
+        return "{} {}".format(obj.price, obj.currency.currency)
+
     class Meta:
         model = ProductPrice
-        fields = ['id', 'url', 'product', 'currency', 'price']
+        fields = ['id', 'url', 'product', 'currency', 'price', 'product_price']
         # expandable_fields = {'product': ProductSerializer, 'format': ProductSizeFormatSetSerializer}
         expandable_fields = {'currency': ProductCurrencySerializer}
